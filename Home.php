8cadbb4191eccd4cd4898469a8adebad9c8b94a2
tfrX4002 <fredryan.toure@gmail.com>
2024-05-15 21:33:25 +0000

    step 1

---
+++ Home.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TRODWE</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="Home.css">
+</head>
+<body>
+<header>
+    <div class="container">
+          <img src="./img/trodwé.png" alt="">
+          <form class="" role="search">
+        <input class="search_bar" type="search" placeholder="Entrez quelque chose" aria-label="Search">
+        <button class="search_bar_button" type="submit">Search</button>
+      </form>
+      <div class="svg">
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+      </svg>
+       <div class= "trait"></div>
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cart-check-fill" viewBox="0 0 16 16">
+        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m-1.646-7.646-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L8 8.293l2.646-2.647a.5.5 0 0 1 .708.708"/>
+      </svg>
+      </div>
+    </div>
+</header>
+  <nav class="navigation">
+      <a href="#">ORDINATEUR</a>
+      <a href="#">TELEPHONE</a>
+      <a href="#">IMPRIMANTE</a>
+      <a href="#">SOURIS</a>
+      <a href="#">TELEVISION</a>
+      <a href="#">CASQUE</a>
+      <a href="#">MEMOIRE</a>
+      <a href="#">CLAVIER</a>
+      <a href="#">CONSOLE</a>
+  </nav>
+  <div class="article">
+    <div class="card-1">
+    <div class="box">
+      Téléphone
+    </div>
+    <div class="box">
+      Ordinateur
+    </div>
+    <div class="box">
+      Console
+    </div>
+    </div>
+    <div class="card-1">
+    <div class="box">
+      Télévision
+    </div>
+    <div class="box">
+      Casque
+    </div>
+    <div class="box">
+      Clavier
+    </div>
+    </div>
+    <div class="card-1">
+    <div class="box">
+      Imprimante
+    </div>
+    <div class="box">
+      Mémoire
+    </div>
+    <div class="box">
+      Souris
+    </div>
+    </div>
+  </div>
+  <footer>
+   <div class="containe">
+        <p>&copy; TRODWE</p>
+   </div>
+  </footer>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
